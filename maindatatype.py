5bc295145877403f776369a4c6491b3a7ce2cff2
Ishitha10 <ishithabommu1@gmail.com>
2024-09-04 20:31:51 +0100

    Add files via upload

---
+++ maindatatype.py
@@ -0,0 +1,14 @@
+a=10
+print(type(a))
+b=int (0.12)
+print(type(b))
+c=True
+print(type(c))
+d="word"
+print(type(d))
+
+e=int(input("Enter a number"))
+print (type(e))
+f="My name is Ishitha"
+print(f[4:8+1])
+print(f[::-1])
